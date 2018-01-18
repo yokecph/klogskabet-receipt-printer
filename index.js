32fd31343a7c097d8edb0ad2785e53f6772c2735
Daniel Høier Øhrgaard <daniel@yoke.dk>
2018-01-18 19:46:51 +0100

    Add comments and clean up a bit

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // Set up the printer
 const printer = require('./lib/printer.js');
 
-printer.on('spooling', _ => console.log('spooling'));
-printer.on('ready', _ => console.log('ready'));
-printer.on('printing', _ => console.log('ready'));
-printer.on('error', (desc) => console.log('error:', desc));
+printer.on('ready', function () {
+  // console.log('ready');
+});
+
+printer.on('error', function (err) {
+  console.error('ERROR', err);
+});
 
 printer.init();
 
-
+// set up the button
 const Button = require('./lib/button.js');
 const printButton = new Button(17);
 
